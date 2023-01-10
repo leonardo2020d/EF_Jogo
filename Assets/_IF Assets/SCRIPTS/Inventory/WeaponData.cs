bd09e9c270f870d646fffe5f53ebff9a14ad147c
leonardo2020d <64494872+leonardo2020d@users.noreply.github.com>
2023-01-10 13:38:47 -0300

    Remocao de componentes

--- Assets/_IF Assets/SCRIPTS/Inventory/WeaponData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Inventory Item Data/Weapon")]
-public class WeaponData : InventoryItemData
-{
-	public int damage;
-}
